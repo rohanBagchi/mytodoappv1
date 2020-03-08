ae8557d43b74f370b16790f946d6583fccdf6ca9
Rohan Bagchi <rohan.bagchi01@gmail.com>
2020-03-08 10:28:13 +0100

    Adding signup hook

---
+++ functions/identity-signup/identity-signup.js
@@ -0,0 +1,65 @@
+// note - this function MUST be named `identity-signup` to work
+// we do not yet offer local emulation of this functionality in Netlify Dev
+//
+// more:
+// https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
+// https://www.netlify.com/docs/functions/#identity-and-functions
+const fetch = require("node-fetch");
+
+exports.handler = async function(event, context) {
+  const data = JSON.parse(event.body)
+  const { user } = data
+
+  const responseBody = {
+    app_metadata: {
+      roles: user.email.split('@')[1] === 'trust-this-company.com' ? ['editor'] : ['visitor'],
+      my_user_info: 'this is some user info'
+    },
+    user_metadata: {
+      ...user.user_metadata, // append current user metadata
+      custom_data_from_function: 'hurray this is some extra metadata'
+    }
+  }
+
+  const requestBodyString = JSON.stringify({
+    query: `
+    mutation insertUser($id: String, $email:String, $name:String){
+      insert_users(objects: {id: $id, email: $email, name: $name}) {
+        affected_rows
+      }
+    }    
+  `,
+    variables: {
+      id: user.id,
+      email: user.email,
+      name: user.user_metadata.full_name
+    }
+  });
+
+  const result = await fetch(
+    "https://hasura-app-with-netlify.herokuapp.com/v1/graphql",
+    {
+      method: "POST",
+      body: requestBodyString,
+      headers: {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret":
+          process.env.HASURA_SECRET
+      }
+    }
+  );
+  const { errors, dataFromHasura } = await result.json();
+
+  if (errors) {
+    console.log(errors);
+    return {
+      statusCode: 500,
+      body: "Something is wrong"
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({...responseBody, ...dataFromHasura})
+    };
+  }
+}
