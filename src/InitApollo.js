7e044b4ee1c94b07cbe0584db3fb533c2714465b
Rohan Bagchi <rohan.bagchi01@gmail.com>
2020-03-15 13:53:06 +0100

    Adding refresh token mechanism and error handling

--- src/InitApollo.js
+++ src/InitApollo.js
@@ -1,17 +1,36 @@
 import ApolloClient from "apollo-boost";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-export const Init = (identity) => {
+let identity;
+
+const forceReload = () => window.location.reload();
+
+export const Init = (identity2) => {
+  identity = identity2;
+
   const customFetch = (uri, options) => {
     const credentials = localStorage.getItem('gotrue.user')
+    if (!credentials) {
+      forceReload();
+    }
+
     const user = credentials && JSON.parse(credentials);
-    const access_token = user?.token?.access_token;
-    const refresh_token = user?.token?.refresh_token;
     const tokenExpiresAt = user?.token?.expires_at;
   
     const hasTokenExpired = new Date() > new Date(tokenExpiresAt);
-  
-    return fetch(uri, options).then(response => response);
+    
+    if (!hasTokenExpired) {
+      return identity
+        .getFreshJWT()
+        .then(r => {
+          return fetch(uri, options).then(response => response);
+        })
+        .catch(e => {
+          identity.logoutUser();
+        });
+    } else {
+      return fetch(uri, options).then(response => response);
+    }
   }
   
   const client = new ApolloClient({
